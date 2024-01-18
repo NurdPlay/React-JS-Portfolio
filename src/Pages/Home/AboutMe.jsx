8d55bcd8fc4be13e0987aea54c8f361e9db6efb4
NurdPlay <142042400+NurdPlay@users.noreply.github.com>
2024-01-18 12:45:49 -0800

    Skills, about me, my portfolio, testimonials, and contact me,  data

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,22 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/about-me.png" alt="About Me" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <p className="section--title">About</p>
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">
+            👋 Hello! I'm John, a passionate and ambitious full stack developer who recently graduated from Nucamp's comprehensive bootcamp in December 2023. Armed with proficiency in the MERN stack (MongoDB, Express.js, React, and Node.js), I am eager to embark on a rewarding journey in the world of web development.
+            </p>
+            <p className="hero--section-description">
+            🚀 My coding adventure began with a curiosity that quickly transformed into a dedicated pursuit of honing my skills. During the bootcamp, I tackled real-world projects, cultivated problem-solving abilities, and fostered a collaborative mindset. My commitment to writing clean and efficient code is matched only by my enthusiasm for staying on the cutting edge of technology trends. Let's build something incredible together! 💻✨
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
